afae0bec586218e233115f247109ef66d8e0d982
fumihiko_hidaka <5.p8\nABYk>
2018-08-27 03:50:10 +0900

    change position

--- app/valid_url.js
+++ app/valid_url.js
@@ -2,16 +2,18 @@ const validator = require('validator');
 const url = require('url');
 
 module.exports = (checkUrl) => {
-  const validOptions = {
-    protocols: ['http', 'https'],
-    require_protocol: true,
-    require_host: true,
-  };
+  if (typeof checkUrl === 'string' && checkUrl.length > 0) {
+    const validOptions = {
+      protocols: ['http', 'https'],
+      require_protocol: true,
+      require_host: true,
+    };
 
-  return (
-    typeof checkUrl === 'string' &&
-    checkUrl.length > 0 &&
-    validator.isURL(url, validOptions) &&
-    validator.isFQDN(checkUrl)
-  );
+    return (
+      validator.isURL(url, validOptions) &&
+      validator.isFQDN(checkUrl)
+    )
+  }
+
+  return false;
 };
