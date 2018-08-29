83cabccabd7c5cb34ff7dcb424bab42b6fc70a1d
fumihiko_hidaka <5.p8\nABYk>
2018-08-30 04:08:48 +0900

    bugfix

--- app/get_puppeteer.js
+++ app/get_puppeteer.js
@@ -5,6 +5,11 @@ module.exports = async (searchOption) => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
 
   try {
+    let checkUrl = searchOption.url;
+    if (checkUrl.indexOf('http') !== 0) {
+      checkUrl = `http://${checkUrl}`;
+    }
+
     const page = await browser.newPage();
 
     await page.setViewport({
@@ -12,7 +17,7 @@ module.exports = async (searchOption) => {
       height: searchOption.height,
     });
 
-    await page.goto(searchOption.url);
+    await page.goto(checkUrl);
 
     // 画面描画が完了するのを待つ
     await new Promise(resolve => setTimeout(() => resolve(), 500));
